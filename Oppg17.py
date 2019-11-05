d09976e5f2869354cf9238084b6d25fe3d044f97
Hans Julian Halvorsen <hans.halvorsen@garderobe.no>
2019-11-05 11:40:27 +0100

    bugfixes, og lagt til et tilfelle med både dirichlet og neumann
    Dirichlet i x = 0, domenet må da også gå fra [0,X], og startbetingelsenene er alpha = 1 i x = 0, beta ellers

--- Oppg17.py
+++ Oppg17.py
@@ -13,9 +13,9 @@ plt.rcParams.update(newparams)
 #CONSTANTS
 mu_g = 1 #5*10**(-5)
 mu_w = 10 #5*10**(-4)
-u =  0 #1.5*10**(-4)
+u =  0.0 #1.5*10**(-4)
 phi = 0.4
-K = 0.1 #10**(-14)
+K = 0.2 #10**(-14)
 P_0 = 5
 
 epsilon = mu_g/mu_w
@@ -32,7 +32,7 @@ def plot_solution(x, t, U, txt='Solution'):
     ax.view_init(azim=30)  # Rotate the figure
     plt.xlabel('t')
     plt.ylabel('x')
-    plt.title(txt);
+    plt.title(txt)
 
 def f1(s):
     return s/(s+(1-s)*epsilon)
@@ -58,7 +58,7 @@ def s_dot_i(s,i,h,f,g):
     gsxx = (g( (s[i+1]+s[i])/2 )*(s[i+1] - s[i]) - g( (s[i]+s[i-1])/2 )*(s[i] - s[i-1]) )/(h**2)
     return (gsxx - fux)/phi
 
-def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True):
+def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True, Neumann = False):
     h = X/n
     t = [0,T]
     if Dirichlet: #dirichletbetingelser
@@ -77,27 +77,42 @@ def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True):
         U_sol[0] = alpha
         U_sol[-1] = beta
         plot_solution(xs,t_list,U_sol)
-    else: #Neumannbetingelser, med den deriverte lik 0
+    if Neumann: #Neumannbetingelser, med den deriverte lik 0
         def s_zero(t,s):
             s0 = np.empty_like(s)
             for i in range(1,len(s0)-1):
                 s0[i] = s_dot_i(s,i,h,f,g)
-            s0[0] = s_dot_i(np.insert(s,0,s[1]),1,h,f,g)
-            s0[-1] = s_dot_i(np.insert(s,-1,s[-2]),-2,h,f,g)
+            s0[0] = s_dot_i(np.insert(s,0,s[0]),1,h,f,g)
+            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g)
             return s0
         sol = solve_ivp(s_zero, t, s, "RK23")
         t_list = sol.t #t-values from the ODE solver
         U_grid = sol.y #U-values from the ODE solver
         plot_solution(xs,t_list,U_grid)
+    else: #dirichlet i X = 0, neumann i enden
+        def s_zero(t,s):
+            s0 = np.empty_like(s)
+            for i in range(1,len(s0)-1):
+                s0[i] = s_dot_i(s,i,h,f,g)
+            s0[0] = s_dot_i(np.insert(s,0,alpha),1,h,f,g)
+            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g)
+            return s0
+        sol = solve_ivp(s_zero, t, s[1:], "RK23")
+        t_list = sol.t #t-values from the ODE solver
+        U_grid = sol.y #U-values from the ODE solver
+        U_sol = np.zeros((n,len(t_list)))
+        U_sol[1:] = U_grid
+        U_sol[0] = alpha
+        plot_solution(xs,t_list,U_sol)
     return 0
 
 X = 10 #length in x-direction
 n = 50 #number of points
 t = 100 #time
-alpha = 0.9
-beta = 0.1 
+alpha = 1
+beta = 0
 
-xs = np.linspace(-X,X,n)
+xs = np.linspace(0,X,n)
 s = np.zeros(n)
 s[xs <= 0] = alpha
 s[xs > 0] = beta
@@ -107,4 +122,4 @@ s2[:] = beta
 s2[abs(xs) <= 3] = alpha
 #print(s2)
 
-solver(X,xs,n,t,f2,g2,s2,alpha,beta,Dirichlet = False)
+solver(X,xs,n,t,f2,g2,s,alpha,beta,Dirichlet = False,Neumann = False)
