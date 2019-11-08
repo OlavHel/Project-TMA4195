ddce6fbd8275fbd391a9b4dff100320e0d554dfe
Hans Julian Halvorsen <hans.halvorsen@garderobe.no>
2019-11-08 17:52:02 +0100

    Scaling changes added

--- Oppg17.py
+++ Oppg17.py
@@ -7,21 +7,11 @@ from scipy.integrate import solve_ivp
 
 newparams = {'figure.figsize': (8.0, 4.0), 'axes.grid': True,
              'lines.markersize': 8, 'lines.linewidth': 2,
-             'font.size': 12}
+             'font.size': 15}
 plt.rcParams.update(newparams)
 
-#CONSTANTS
-mu_g = 5*10**(-5)
-mu_w = 5*10**(-4)
-u =  1.5*10**(-6)
-phi = 0.4
-K = 10**(-13)
-P_0 = 7.5*10**6
-
-epsilon = mu_g/mu_w
-
 #function for 3D plotting
-def plot_solution(x, t, U, txt='Solution'):
+def plot_solution(x, t, U, txt='$S_g(x,t)$'):
     # Plot the solution of the heat equation
     fig = plt.figure()
     plt.clf()
@@ -30,9 +20,28 @@ def plot_solution(x, t, U, txt='Solution'):
     # ax.plot_wireframe(T, X, U)
     ax.plot_surface(T, X, U, cmap=cm.coolwarm)
     ax.view_init(azim=30)  # Rotate the figure
-    plt.xlabel('t')
-    plt.ylabel('x')
-    plt.title(txt)
+    plt.xlabel('$t$',fontsize = 24)
+    plt.ylabel('$x$',fontsize = 24)
+    plt.title(txt, fontsize = 24)
+
+#CONSTANTS
+mu_g = 5*10**(-5) #
+mu_w = 5*10**(-4) # Pascal*s
+u =   1.5*10**(-6) #
+phi = 0.4 #dimensionless
+K = 10**(-13) # m**2
+P_0 = 7.5*10**5 #Pascal, N/m**2
+
+#Dimensionless constants
+
+def dimless(X):
+    bigL = X
+    gamma = K*P_0/mu_w # Pa*m**2 / Pa*s = m**2 / s
+    nu = bigL*u/gamma # m*(m/s) / ()
+    bigT = bigL**2*phi/gamma
+    return nu,bigT
+
+epsilon = mu_g/mu_w
 
 def f1(s):
     return s/(s+(1-s)*epsilon)
@@ -40,27 +49,40 @@ def f1(s):
 def f2(s):
     return s**2/(s**2+epsilon*(1-s)**2)
 
-# f3 og g3 er kun for å teste script, gir transport equation med travelling waves for riktige startvilkår
 def f3(s):
-    return s
-
-def dpc(s):
-    return -P_0
+    return 0
 
 def g1(s):
-    return -f1(s)*K*(1-s)/mu_w*dpc(s)
+    return f1(s)*(1-s)
 
 def g2(s):
-    return -f2(s)*K*(1-s)**2/mu_w*dpc(s)
+    return f2(s)*(1-s)**2
 
 def g3(s):
     return 0
 
-def s_dot_i(s,i,h,f,g):
+def fgplotter():
+    n = 100
+    xs = np.linspace(0,1,n)
+    plt.figure('f1')
+    plt.plot(xs,f1(xs))
+    plt.show()
+    plt.figure('g1')
+    plt.plot(xs,g1(xs))
+    plt.show()
+    plt.figure('f2')
+    plt.plot(xs,f2(xs))
+    plt.show()
+    plt.figure('g2')
+    plt.plot(xs,g2(xs))
+    plt.show()        
+
+def s_dot_i(s,i,h,f,g,X):
     
-    fux = u*( f(s[i+1])-f(s[i-1]) )/(2*h)
+    fux = ( f(s[i+1])-f(s[i-1]) )/(2*h)
     gsxx = (g( (s[i+1]+s[i])/2 )*(s[i+1] - s[i]) - g( (s[i]+s[i-1])/2 )*(s[i] - s[i-1]) )/(h**2)
-    return (gsxx - fux)/phi
+    nu,bigT = dimless(X)
+    return gsxx - nu*fux
 
 def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True, Neumann = False):
     h = X/n
@@ -69,9 +91,9 @@ def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True, Neumann = False):
         def s_zero(t,s):
             s0 = np.empty_like(s)
             for i in range(1,len(s0)-1):
-                s0[i] = s_dot_i(s,i,h,f,g)
-            s0[0] = s_dot_i(np.insert(s,0,alpha),1,h,f,g)
-            s0[-1] = s_dot_i(np.insert(s,-1,beta),-2,h,f,g)
+                s0[i] = s_dot_i(s,i,h,f,g,X)
+            s0[0] = s_dot_i(np.insert(s,0,alpha),1,h,f,g,X)
+            s0[-1] = s_dot_i(np.insert(s,-1,beta),-2,h,f,g,X)
             return s0
         sol = solve_ivp(s_zero, t, s[1:-1], "RK23")
         t_list = sol.t #t-values from the ODE solver
@@ -80,26 +102,26 @@ def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True, Neumann = False):
         U_sol[1:-1] = U_grid
         U_sol[0] = alpha
         U_sol[-1] = beta
-        plot_solution(xs,t_list,U_sol)
+        plot_solution(xs,t_list/T,U_sol)
     elif Neumann: #Neumannbetingelser, med den deriverte lik 0
         def s_zero(t,s):
             s0 = np.empty_like(s)
             for i in range(1,len(s0)-1):
-                s0[i] = s_dot_i(s,i,h,f,g)
-            s0[0] = s_dot_i(np.insert(s,0,s[0]),1,h,f,g)
-            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g)
+                s0[i] = s_dot_i(s,i,h,f,g,X)
+            s0[0] = s_dot_i(np.insert(s,0,s[0]),1,h,f,g,X)
+            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g,X)
             return s0
         sol = solve_ivp(s_zero, t, s, "RK23")
         t_list = sol.t #t-values from the ODE solver
         U_grid = sol.y #U-values from the ODE solver
-        plot_solution(xs,t_list,U_grid)
+        plot_solution(xs,t_list/T,U_grid)
     else: #dirichlet i X = 0, neumann i enden
         def s_zero(t,s):
             s0 = np.empty_like(s)
             for i in range(1,len(s0)-1):
-                s0[i] = s_dot_i(s,i,h,f,g)
-            s0[0] = s_dot_i(np.insert(s,0,alpha),1,h,f,g)
-            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g)
+                s0[i] = s_dot_i(s,i,h,f,g,X)
+            s0[0] = s_dot_i(np.insert(s,0,alpha),1,h,f,g,X)
+            s0[-1] = s_dot_i(np.insert(s,-1,s[-1]),-2,h,f,g,X)
             return s0
         sol = solve_ivp(s_zero, t, s[1:], "RK23")
         t_list = sol.t #t-values from the ODE solver
@@ -107,13 +129,13 @@ def solver(X,xs,n,T,f,g,s,alpha,beta,Dirichlet = True, Neumann = False):
         U_sol = np.zeros((n,len(t_list)))
         U_sol[1:] = U_grid
         U_sol[0] = alpha
-        plot_solution(xs,t_list,U_sol)
+        plot_solution(xs,t_list/T,U_sol)
     return 0
 
 def question7():    
-    X = 1 #length in x-direction
-    n = 100 #number of points
-    T = 50 #time
+    X = 2.3 #x* = X times dimentionless x x* = Lx
+    n = 50 #number of points
+    T = 1 #time
     alpha = 1
     beta = 0
     
@@ -124,21 +146,23 @@ def question7():
     solver(X,xs,n,T,f1,g1,s,alpha,beta,Dirichlet = False,Neumann = False)
 
 def question17(case1 = True):
-    X = 1 #length in x-direction
-    n = 100 #number of points
-    T = 100 #time
+    n = 50 #number of points
+    T = 1 #time
     alpha = 1 #initial vales
     beta = 0 # --||--
-    xs = np.linspace(0,X,n)
-    if case1:       
+    if case1:
+        X = 1 #x* = X times dimentionless x
+        xs = np.linspace(0,X,n)
         b0 = 0 #boundary values
         b1 = 0 #--||--
         s = np.zeros(n)
         s[:] = beta
         s[int(len(xs)*0.47):int(len(xs)*0.53)] = alpha
         
-        solver(X,xs,n,T,f2,g2,s,b0,b1,Dirichlet = False,Neumann = True)
+        solver(X,xs,n,T,f3,g2,s,b0,b1,Dirichlet = False,Neumann = True)
     else:
+        X = 1.15 #x* = X times dimentionless x
+        xs = np.linspace(0,X,n)
         b0 = 1 #boundary values
         b1 = 0 #--||--       
         s = np.zeros(n)
@@ -147,8 +171,7 @@ def question17(case1 = True):
         
         solver(X,xs,n,T,f2,g2,s,b0,b1,Dirichlet = False,Neumann = False)
 
+#fgplotter()
 question7()
-
 question17()
-    
 question17(False)
